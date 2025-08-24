ba915452e16f5fe27a471b14a9a929a121716a84
Deependra2007 <2023ad05028@wilp.bits-pilani.ac.in>
2025-08-24 22:08:59 +0530

    Update convai_group_111_rag_vs_ft.py

--- convai_group_111_rag_vs_ft.py
+++ convai_group_111_rag_vs_ft.py
@@ -260,7 +260,7 @@ from pathlib import Path
 
 # Example: set a path inside your Google Drive
 temp_dir =  tempfile.TemporaryDirectory()
-drive_path = pathlib.Path(temp_dir.name)
+drive_path = Path(temp_dir.name)
 #Path('/content/drive/MyDrive/ConvAI_Group_111_RAG')  # create or use a folder in MyDrive
 #drive_path.mkdir(parents=True, exist_ok=True)  # make sure the folder exists
 
