d0eb53dc02e41ea426c32e1b44300525bc1671aa
Deependra2007 <2023ad05028@wilp.bits-pilani.ac.in>
2025-08-24 11:48:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,19 +4,19 @@ import Finetune
 
 st.title("Choose Model for Execution")
 # Initialize session state
-#if "model" not in st.session_state:
-   # st.session_state.model = None
+if "model" not in st.session_state:
+    st.session_state.model = None
 option = st.selectbox(
     "Select an Option",
     ["Select...", "RAG", "Fine Tune"]
 )
 
-#if option is not None:
-  #  st.session_state.model = option
-if st.button("Execute"):
-    if option == "RAG":
+if option is not None:
+    st.session_state.model = option
+if st.button("Execute") or st.session_state.model is not None:
+    if st.session_state.model == "RAG":
         rag_ui.run()
-    elif option == "Fine Tune":
+    elif st.session_state.model == "Fine Tune":
         Finetune.run()
     else:
         st.warning("Please select a valid option before clicking Run.")
